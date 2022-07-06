121a60b5bc9432eb8f78681ac26031bc71d203b9
Artemides <edmpulasky@gmail.com>
2022-07-06 15:34:19 -0500

    ready

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,9 +6,7 @@ const options= {
   dialect: 'postgres',
   logging: config.isProd ? false:true,
 }
-if(config.isProd){
-  options.dialectOptions.ssl={rejectUnauthorized:false}
-}
+if(config.isProd) options.dialectOptions={ssl: {rejectUnauthorized:false}}
 const sequelize = new Sequelize(config.dbUrl, {
   dialect: 'postgres',
   logging: false,
